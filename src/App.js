3781672411af60e8fa3c6cb939402e89129801fe
victor <vmejiaec@gmail.com>
2020-01-14 08:27:57 -0500

    cel bueno

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Saludo from './Saludo';
+import Propiedad from './propiedad';
 
 function App() {
   return (
     <div className="App">
       <Saludo></Saludo>
+      <Propiedad saldo='18'/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
